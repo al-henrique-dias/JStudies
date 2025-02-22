bec8f7cf6f264cbb60c14230ff739e1819a5da68
Henrique (º-º) <87720975+al-henrique-dias@users.noreply.github.com>
2025-02-22 17:40:33 -0400

    I created a Random password generator
    This is anexercise for functions and random numbers

--- index.js
+++ index.js
@@ -495,3 +495,50 @@ function combineStrings(...strings){
     return strings.join(" ");
 }
 console.log(combineStrings("Mr.", "Spongebob", "Squarepants", "III"));
+
+//Random password generator (exercise for functions and random numbers)
+console.log("");//line separator
+function generatePassword(length, includeLowercase, includeUppercase, includeNumbers, includeSymbols){
+
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%¨&*()_+"
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += (includeLowercase) ? lowercaseChars : "";//reads if(includeLowercase) ? then allowedChars += lowercaseChars, else : allowedChars += ""
+    allowedChars += (includeUppercase) ? uppercaseChars : "";
+    allowedChars += (includeNumbers) ? numberChars : "";
+    allowedChars += (includeSymbols) ? symbolChars : "";
+
+    if(length <= 0)
+        return `(password length must be at least 1)`;
+    if(allowedChars.length === 0){
+        return `(at least 1 set of character must be selected)`;
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length)
+        password += allowedChars[randomIndex];
+    }
+
+    return password;
+
+}
+
+const passLength = 10;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(
+    passLength,
+    includeLowercase,
+    includeUppercase,
+    includeNumbers,
+    includeSymbols
+);
+console.log(`Generated password: ${password}`);
