44b54d3a00843b2b2bc9170d362bc2691f9e2277
Henrique Dias <henqrique.180803@gmail.com>
2025-02-22 01:28:18 -0400

    brand new start on javascript studies

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log(`Hello world\nI like pizza`);//displays something at console
+//window.alert(`This is an alert\nI like pizza!`);//displays something at the user screen through browser (over everything)
+
+/**
+ * documente works like a class and has a lot of methods to work with
+ * getElementById(elementId: String) returns an HTMLElement
+ */
+document.getElementById("title").textContent = "Hello world";
+document.getElementById("text").textContent = "I like pizza";
+
